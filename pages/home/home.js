89beba10f54d111afddfb9424afc1ec34bb5a4b3
FabioAbbud <fabioabbud98@hotmail.com>
2023-11-29 19:27:40 -0300

    implementaçao api

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,5 +1,31 @@
 function myHome() {
     changeTitle();
     console.log('home funciona');
+
+    // Obtém todos os 'item'.
+    getAll('/items', '#tableItem');
+
+    // Obtém todos os 'owner'.
+    getAll('/owner', '#tableOwner');
+
 }
+
+function getAll(endPoint, tableId) {
+
+    // Acessa o endpoint da API.
+    $.get(app.apiBaseURL + endPoint)
+
+        // Se deu certo.
+        .done((apiData) => {
+            console.log("Dados:", apiData);
+        })
+
+        // Se falhou.
+        .fail((error) => {
+            console.error("Oooops! ", error)
+        })
+
+    return false;
+}
+
 $(document).ready(myHome);
